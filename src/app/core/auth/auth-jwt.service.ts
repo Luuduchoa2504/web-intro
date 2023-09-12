4c412c8db313a0ead29745f680e1179ff7470565
hoaluu <hoa_luu@datahouse.com>
2023-09-12 11:38:49 +0700

    add auth-jwt and auth-interceptor

--- src/app/core/auth/auth-jwt.service.ts
+++ src/app/core/auth/auth-jwt.service.ts
@@ -1,9 +1,57 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { LocalStorageService, SessionStorageService } from "ngx-webstorage";
+import { Constants } from "../../helpers";
+import { Login } from "../../components/sign-in/login.model";
+import { map, Observable } from "rxjs";
+
+type JwtToken = {
+  id_token: string;
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthJwtService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private $localStorage: LocalStorageService,
+    private $sessionStorage: SessionStorageService,
+  ) { }
+
+  getToken(): string {
+    const tokenInLocalStorage: string | null = this.$localStorage.retrieve(
+      Constants.KEY_TOKEN
+    );
+    const tokenInSessionStorage: string | null = this.$sessionStorage.retrieve(
+      Constants.KEY_TOKEN
+    );
+    return tokenInLocalStorage ?? tokenInSessionStorage ?? '';
+  }
+
+  login(credentials: Login): Observable<void> {
+    return this.http
+      .post<JwtToken>(
+        Constants.SERVER_API_URL + 'api/login',
+        //hardcode rememberMe to false
+        {...credentials, rememberMe: false}
+      )
+      .pipe(
+        map((response) =>
+          this.authenticateSuccess(response, credentials.rememberMe)
+        )
+      );
+  }
+
+  private authenticateSuccess(response: JwtToken, rememberMe: boolean): void {
+    const jwt = response.id_token;
+    if (rememberMe) {
+      this.$localStorage.store(Constants.KEY_TOKEN, jwt);
+      this.$sessionStorage.clear(Constants.KEY_TOKEN);
+    } else {
+      this.$sessionStorage.store(Constants.KEY_TOKEN, jwt);
+      this.$localStorage.clear(Constants.KEY_TOKEN);
+    }
+  }
 }
--- src/app/core/interceptor/auth.interceptor.ts
+++ src/app/core/interceptor/auth.interceptor.ts
@@ -6,13 +6,31 @@ import {
   HttpInterceptor
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {LocalStorageService, SessionStorageService} from "ngx-webstorage";
+import {Constants} from "../../helpers";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
+  private readonly SERVER_API_URL: any;
 
-  constructor() {}
+  constructor(private localStorage: LocalStorageService, private sessionStorage: SessionStorageService) {
+    this.SERVER_API_URL = Constants.SERVER_API_URL;
+  }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!request || !request.url || (request.url.startsWith('http')
+      && !(Constants.SERVER_API_URL && request.url.startsWith(Constants.SERVER_API_URL)))) {
+      return next.handle(request);
+    }
+
+    const token: string | null = this.localStorage.retrieve(Constants.KEY_TOKEN) ?? this.sessionStorage.retrieve(Constants.KEY_TOKEN);
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    }
     return next.handle(request);
   }
 }
