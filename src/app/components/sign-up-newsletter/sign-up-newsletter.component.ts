27a781707a0dcf15b37bab7f3076082a53acbbdc
hoaluu <hoa_luu@datahouse.com>
2023-09-18 00:31:14 +0700

    add email send to receive notification validate

--- src/app/components/sign-up-newsletter/sign-up-newsletter.component.ts
+++ src/app/components/sign-up-newsletter/sign-up-newsletter.component.ts
@@ -1,6 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {SendMailService} from "./services/send-mail.service";
+import {CustomToastService} from "../../shared/utils/custom-toast.service";
+import {Router} from "@angular/router";
+import {ToastrService} from "ngx-toastr";
 // import {ToastrService} from "ngx-toastr";
 
 @Component({
@@ -10,23 +13,31 @@ import {SendMailService} from "./services/send-mail.service";
 })
 export class SignUpNewsletterComponent implements OnInit {
   submitForm: FormGroup = this.fb.group({
-    email: [null,[Validators.email]],
+    email: [null,[Validators.required, Validators.email]],
   });
 
   constructor(
     private fb: FormBuilder,
     private sendMailService: SendMailService,
-    // private toastr: ToastrService
+    private toastr: ToastrService,
+    private router: Router,
   ) {}
 
   ngOnInit() {
   }
 
   send() {
-    // this.signupForm.valid ? this.toastr.success('Gửi thành công')
-    //   : (this.signupForm.value.email ? this.toastr.error('Sai định dạng email')
-    //     : this.toastr.warning('Vui lòng nhập email'));
-    const email = this.submitForm.get('email')?.value;
-    this.sendMailService.send({email}).subscribe();
+    this.submitForm.valid ? this.toastr.success('thành công', 'Gửi thành công')
+      : (this.submitForm.value.email ? this.toastr.error('Lỗi','Sai định dạng email')
+        : this.toastr.warning('Cảnh báo','Vui lòng nhập email'));
+    if (this.submitForm.valid) {
+      const email = this.submitForm.get('email')?.value;
+      this.sendMailService.send({email}).subscribe((res) => {
+        if (res) {
+          this.submitForm.reset();
+          this.router.navigate(['/']);
+        }
+      });
+    }
   }
 }
